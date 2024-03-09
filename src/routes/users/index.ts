3d35218a3a15020f1f0d1b7430c3d2f731ca9c1f
Abdelrahman Sadat <abdelrahman.sadat@ithraa.sa>
2024-03-09 06:17:33 +0200

    - feat: fuzzy search

--- src/routes/users/index.ts
+++ src/routes/users/index.ts
@@ -1,34 +1,53 @@
 import { Type as t } from "@fastify/type-provider-typebox";
 import { FastifyPluginAsync } from "fastify";
+import lunr from "lunr";
 
 const getUsers: FastifyPluginAsync = async (
   fastify // since we're using the type provider `FastifyPluginAsync` we don't need to declare argument/return/function types
 ) => {
-  fastify.get<{ Querystring: Partial<User> }>(
+  fastify.get<{ Querystring: QueryString }>(
     "/",
     {
-      schema: { // our api schema
+      schema: {
+        // our api schema
         querystring: t.Object({
           id: t.Optional(t.Number()),
           name: t.Optional(t.String()),
           age: t.Optional(t.Number()),
           active: t.Optional(t.Boolean()),
-          last_login: t.Optional(t.Unsafe<Date>({ type: "string", format: "date" })),
+          last_login: t.Optional(
+            t.Unsafe<Date>({ type: "string", format: "date" })
+          ),
+          search_name: t.Optional(t.String()),
         }),
       },
     },
     async (request, reply) => {
       const users = await readUsers();
-      const filteredUsers = filterUsers(users, request.query);
-      const sortedFilteredUsers = sortUsers(filteredUsers, "last_login");
+      const {search_name: searchName, ...params} = request.query;
+      const filteredUsers = filterUsers(users, params);
+      let sortedFilteredUsers = sortUsers(filteredUsers, "last_login");
+      if (searchName) {
+        sortedFilteredUsers = searchByName(
+          sortedFilteredUsers,
+          searchName
+        );
+      }
       const userStats = userStatAggregator(sortedFilteredUsers);
       return reply.send({ users: sortedFilteredUsers, stats: userStats });
     }
   );
 };
-// fastify functions do look kinda ugly tho (callback hellish)... But ig one can get used to it and abstract a bit out.
 
 export default getUsers;
+
+
+
+
+// I opted to keep utility funcs and type definitions in the same file for simplicity's sake
+// =================================================================================================
+// Utility functions
+// =================================================================================================
 
 const filterUsers: (users: User[], params: Partial<User>) => User[] = (
   users,
@@ -46,23 +65,24 @@ const sortUsers: (users: User[], sortBy: keyof User) => User[] = (
   sortBy
 ) => {
   return users.sort((a, b) => {
-    if (a[sortBy] < b[sortBy])
-      return -1;
-    if (a[sortBy] > b[sortBy])
-      return 1;
+    if (a[sortBy] < b[sortBy]) return -1;
+    if (a[sortBy] > b[sortBy]) return 1;
     return 0;
   });
 };
 
 const userStatAggregator = (users: User[]) => {
-    const reducer = (acc: { activeUsers: number; avgAge: number }, curr: User) => {
-        acc.activeUsers += curr.active ? 1 : 0;
-        acc.avgAge = (acc.avgAge * (users.length - 1) + (+curr.age)) / users.length;
-        return acc;
-    };
-    const aggregate = users.reduce(reducer, { activeUsers: 0, avgAge: 0 });
-    return aggregate
-}
+  const reducer = (
+    acc: { activeUsers: number; avgAge: number },
+    curr: User
+  ) => {
+    acc.activeUsers += curr.active ? 1 : 0;
+    acc.avgAge = (acc.avgAge * (users.length - 1) + +curr.age) / users.length;
+    return acc;
+  };
+  const aggregate = users.reduce(reducer, { activeUsers: 0, avgAge: 0 });
+  return aggregate;
+};
 
 const readUsers = (): Promise<User[]> => {
   return new Promise((resolve, reject) => {
@@ -79,6 +99,31 @@ const readUsers = (): Promise<User[]> => {
   });
 };
 
+// ! This is not suitable for production. `lunr` is a good use for static docs
+// ! I use it here since users are in a static file
+const searchByName = (users: User[], searchWord: string) => {
+  // ? idx for static files could be cached
+  var idx = lunr(function () {
+    this.ref("id");
+    this.field("name");
+    users.forEach(function (user) {
+      // @ts-ignore
+      this.add(user);
+    }, this);
+  });
+
+  // fuzzy matching degree can be customized
+  const searchResultIds = idx.search(`${searchWord}~2`).map((item) => +item.ref);
+
+  return users.filter((user) => searchResultIds.includes(+user.id));
+};
+
+
+
+// =================================================================================================
+// Types
+// =================================================================================================
+
 interface User {
   [key: string]: any;
   id: number | string;
@@ -86,4 +131,9 @@ interface User {
   age: number | string;
   active: boolean;
   last_login: Date;
+  search_name: string;
+}
+
+interface QueryString extends Partial<User> {
+  search_name?: string;
 }
